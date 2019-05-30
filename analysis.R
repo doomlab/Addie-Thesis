0ae2853cdf1032f8d1d03b5c4ba59883c2cce340
addwiko <35707681+addwiko@users.noreply.github.com>
2019-05-30 13:54:17 -0500

    updates

--- analysis.R
+++ analysis.R
@@ -12,6 +12,10 @@ summary(master)
 mean(master$Ravens.Score, na.rm = TRUE)
 sd(master$Ravens.Score, na.rm = TRUE)
 
+#Typing
+mean(master$TTScore, na.rm = TRUE)
+sd(master$TTScore, na.rm = TRUE)
+
 #OSPAN
 mean(master$Letter.Recall, na.rm = TRUE)
 sd(master$Letter.Recall, na.rm = TRUE)
@@ -20,9 +24,16 @@ sd(master$Letter.Recall, na.rm = TRUE)
 mean(master$Math.Acc, na.rm = TRUE)
 sd(master$Math.Acc, na.rm = TRUE)
 
-#Typing
-mean(master$TTScore, na.rm = TRUE)
-sd(master$TTScore, na.rm = TRUE)
+##Dataset to get means for those ONLY above 85%
+OSPAN = subset(master, Math.Acc >=85)
+
+#OSPAN
+mean(OSPAN$Letter.Recall, na.rm = TRUE)
+sd(OSPAN$Letter.Recall, na.rm = TRUE)
+
+#Math Acc
+mean(OSPAN$Math.Acc, na.rm = TRUE)
+sd(OSPAN$Math.Acc, na.rm = TRUE)
 
 ##cor.test
 
@@ -36,28 +47,30 @@ cor.test(master$Ravens.Score, master$TTScore)
 cor.test(master$Letter.Recall, master$TTScore)
 
 #power test
+library(pwr)
 pwr.r.test(n = 41, r = .22, alternative = "two.sided")
 pwr.r.test(n = NULL, r = .22, alternative = "two.sided",
            power = .80)
 
-##regression (fix column names) 
+##regression 
 model = lm(Ravens.Score ~ Letter.Recall + Math.Acc,  data = master)
 summary(model)
 
 library(lm.beta)
 summary.lm.beta(model) #to get standardized coefficients
+lm.beta(model)
 
 library(ppcor)
 pcor(na.omit(master[ , c("Letter.Recall", "Math.Acc", "Ravens.Score")]))
 
-model4 = lm(TTScore ~ Letter.Recall + Math.Acc, data = master)
-summary(model4)
-summary.lm.beta(model4)
+model2 = lm(TTScore ~ Letter.Recall + Math.Acc, data = master)
+summary(model2)
+summary.lm.beta(model2)
 pcor(na.omit(master[ , c("Letter.Recall", "Math.Acc", "TTScore")]))
 
-model5 = lm(TTScore ~ Letter.Recall + Ravens.Score + Math.Acc, data = master)
-summary(model5)
-summary.lm.beta(model5)
+model3 = lm(TTScore ~ Letter.Recall + Ravens.Score + Math.Acc, data = master)
+summary(model3)
+summary.lm.beta(model3)
 pcor(na.omit(master[ , c("Letter.Recall", "Math.Acc", "Ravens.Score", "TTScore")]))
 
 # model6 = lm(TTScore ~ Letter.Recall * Ravens.Score + Math.Acc, data = master)
